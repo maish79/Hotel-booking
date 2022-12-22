73423c921f49956ae4b164ec083bb6f7eb149620
maish79 <maishsupa@yahoo.com>
2022-12-22 20:21:48 +0000

    modified models.py, add room class,types and capacity

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    Room_types=(('one', 'single'),
+                ('two', 'double'),
+                ('multi','family'),
+    )
+    category = models.CharField(choices=Room_types, max_length=6)
+    number = models.IntegerField()
+    capacity = models.IntegerField()
+    beds = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.number}. {self.category} with {self.beds} beds for {self.capacity} people'
+
