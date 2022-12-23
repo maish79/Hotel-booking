2e9227e085020807c11819e19fb38edc19556a70
maish79 <maishsupa@yahoo.com>
2022-12-23 22:48:43 +0000

    linked static file to room_list.html

--- hotel/forms.py
+++ hotel/forms.py
@@ -1,12 +1,5 @@
 from django import forms
 
 class AvailabilityForm(forms.Form):
-    ROOM_CATEGORIES = (
-            ('one', 'single'),
-            ('two', 'double'),
-            ('multi','family'),
-        )
-
-    room_category = forms.ChoiceField(choices=ROOM_CATEGORIES, required=True)
-    check_in = forms.DateTimeField(required=True, input_formats=("%Y%m%d-%H%M%S"))
-    check_out = forms.DateTimeField(required=True, input_formats=("%Y%m%d-%H%M%S"))
+    check_in = forms.DateTimeField(required=True, input_formats=["%Y-%m-%dT%h:%M",])
+    check_out = forms.DateTimeField(required=True, input_formats=["%Y-%m-%dT%h:%M",])
--- hotel/views.py
+++ hotel/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView, FormView, View
+from django.urls import reverse
 from .models import Room, Booking
 from .forms import AvailabilityForm
 from hotel.booking_func.availability import check_availability
 
 
+
 # Create your views here.
-class RoomListView(ListView):
-    model = Room
+def RoomListView(request):
+    room = Room.objects.all()[0]
+    room_categories= dict(room.ROOM_CATEGORIES)
+
+    context= {}
+    return render(request, 'room_list_view.html', context)
 
 class BookingList(ListView):
     model = Booking
@@ -15,6 +21,7 @@ class BookingList(ListView):
 class RoomDetailView(View):
     def get(self, request,*args, **kwargs):
         category = self.kwargs.get('category', None)
+        form =AvailabilityForm()
         room_list = Room.objects.filter(category=category)
         
         
@@ -22,7 +29,8 @@ class RoomDetailView(View):
             room = room_list[0]
             room_category = dict(room.ROOM_CATEGORIES).get(room.category, None)
             context = {
-                'room_category': room_category
+                'room_category': room_category,
+                'form': form,
             }
             return render(request, 'room_detail_view.html', context)
         else: 
@@ -30,18 +38,24 @@ class RoomDetailView(View):
 
 
     def post(self, request, *args, **kwargs):
+        category = self.kwargs.get('category', None)
         room_list = Room.objects.filter(category=category)
+        form = AvailabilityForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
         available_rooms = []
         for room in room_list:
-            if check_availability(room,data['check_in']. data['check_out']):
+            if check_availability(room, data['check_in'], data['check_out']):
                 available_rooms.append(room)
         
-        if len(avaiable_rooms)>0:
+        if len(available_rooms) > 0:
             room= available_rooms[0]
-            booking = booking.objects.create(
+            booking = Booking.objects.create(
                 user = request.user,
                 room = room,
-                check_in= data['check_in'],
+                check_in = data['check_in'],
                 check_out=data['check_out']
             )
             booking.save()
